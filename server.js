87269b2f0bc60a02dd04076f81f73a9f3a1f0950
wzahrt <wzahrt@unc.edu>
2023-04-26 22:23:56 -0400

    fixed install:

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import express from 'express';
-import minimist from 'minimist';
+import express from "express"
+import minimist from "minimist"
 import { rps, rpsls } from './lib/rpsls.js'
 
 const app = express()
