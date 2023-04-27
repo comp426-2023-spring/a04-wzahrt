df291b5484611963008c95a45345f1c43ac7019a
wzahrt <wzahrt@unc.edu>
2023-04-26 22:19:44 -0400

    forgot to create const app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import minimist from 'minimist';
 import { rps, rpsls } from './lib/rpsls.js'
 
+const app = express()
 var args = minimist(process.argv.slice(2));
 const PORT = args.port || 5000;
 
