a81ca06603a4d7bfcc427390887eaeff4a8ca8af
wzahrt <wzahrt@unc.edu>
2023-04-26 22:16:24 -0400

    I think i finished?

--- server.js
+++ server.js
@@ -2,3 +2,53 @@
 import express from 'express';
 import minimist from 'minimist';
 import { rps, rpsls } from './lib/rpsls.js'
+
+var args = minimist(process.argv.slice(2));
+const PORT = args.port || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/app', (req, res) => {
+	res.status(200).send("200 OK");
+})
+
+app.get('/app/rps', (req, res) => {
+	res.status(200).send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+	res.status(200).send(rpsls());
+})
+
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.query.shot));
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+	res.status(200).send(rpsls(req.query.shot));
+})
+
+app.get('/app/rps/play/:arg', (req, res) => {
+	res.status(200).send(rps(req.params.arg));
+})
+
+app.get('/app/rpsls/play/:arg', (req, res) => {
+	res.status(200).send(rpsls(req.params.arg));
+})
+
+app.post('/app/rps/play', (req, res) => {
+        res.status(200).send(rps(req.body.shot));
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+        res.status(200).send(rpsls(req.body.shot));
+})
+
+app.get('*', (req, res) => {
+	res.status(404).send('404 NOT FOUND');
+})
+
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
+})
